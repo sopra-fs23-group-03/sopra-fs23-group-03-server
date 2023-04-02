6fea4100a49d4c1f574da6c3375cbdeaf89f93d8
OrestisBollano <orestisbollano9@gmail.com>
2023-04-02 20:08:29 +0300

    logout method created

--- src/main/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepository.java
@@ -11,5 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   User findByUsername(String username);
 
-    Optional<User> findById(Long id);
+  Optional<User> findById(Long id);
 }
