ce1966ad88c56fcaa3d645ac7556cd0d8d16e47b
Lany Weizenblut <lany1997@hotmail.com>
2023-04-06 17:06:48 +0200

    added getUseridByToken to UserService

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/UserControllerTest.java
@@ -18,7 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders; //unused
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collections;
