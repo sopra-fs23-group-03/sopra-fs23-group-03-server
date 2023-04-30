f866846f68a145aa061fcbe9f47cd986c37db4da
Lany Weizenblut <lany1997@hotmail.com>
2023-04-30 12:08:39 +0200

    unnused import removal

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/UserService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.Optional;
