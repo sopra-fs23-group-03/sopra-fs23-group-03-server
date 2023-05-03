57eaf7f4cddf45479bee9e91cea1374f5d9a56d8
Lany Weizenblut <lany1997@hotmail.com>
2023-05-03 15:42:40 +0200

    removed left-over comment

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/GroupController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/GroupController.java
@@ -242,12 +242,7 @@ public class GroupController {
         }
 
         // retrieve all ingretients available from the members of the group
-        Set<Ingredient> groupIngredients = group.getIngredients(); // retrieve directly from group now
-        // for(Long memberId : memberIds) {
-        //     User user = userService.getUserById(memberId);
-        //     Set<Ingredient> userIngredients = user.getIngredients();
-        //     groupIngredients.addAll(userIngredients);
-        // }
+        Set<Ingredient> groupIngredients = group.getIngredients();
 
         // convert to API representation
         List<IngredientGetDTO> ingredientGetDTOs = new ArrayList<>();
