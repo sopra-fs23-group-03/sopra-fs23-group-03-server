ea39330d235fefc51213a6c439358eed8f9a52c3
akkuebler <91547040+akkuebler@users.noreply.github.com>
2023-05-11 23:10:36 +0200

    tests for recipeService

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/APIControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/APIControllerTest.java
@@ -1,95 +1,95 @@
-//package ch.uzh.ifi.hase.soprafs23.controller;
-//
-//import ch.uzh.ifi.hase.soprafs23.SpooncularAPI.*;
-//import ch.uzh.ifi.hase.soprafs23.entity.Group;
-//import ch.uzh.ifi.hase.soprafs23.entity.User;
-//import ch.uzh.ifi.hase.soprafs23.service.GroupService;
-//import ch.uzh.ifi.hase.soprafs23.service.UserService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.web.client.HttpClientErrorException;
-//import org.springframework.web.client.RestTemplate;
-//import org.springframework.web.server.ResponseStatusException;
-//import org.springframework.web.util.NestedServletException;
-//
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.ArrayList;
-//
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.hamcrest.Matchers.hasSize;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.ArgumentMatchers.anyLong;
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.doReturn;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@Import(RestTemplateConfig.class)
-//public class APIControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private GroupService groupService;
-//
-//    @MockBean
-//    private UserService userService;
-//
-//    @MockBean
-//    private APIService apiService;
-//    @Mock
-//    private RestTemplate restTemplate;
-//    @InjectMocks
-//    private APIController apiController;
-//
-//    private Group testGroup;
-//    private User testUser;
-//    private Recipe testRecipe;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        testGroup = new Group();
-//        testGroup.setId(1L);
-//        testGroup.setHostId(1L);
-//
-//        testUser = new User();
-//        testUser.setId(1L);
-//
-//        testRecipe = new Recipe();
-//        testRecipe.setId(1);
-//        testRecipe.setTitle("Test Recipe");
-//        testRecipe.setReadyInMinutes(30);
-//        testRecipe.setPricePerServing(5.00);
-//
-//        doReturn(testGroup).when(groupService).getGroupById(anyLong());
-//        doReturn(testUser).when(userService).getUserById(anyLong());
-//        doReturn(1L).when(userService).getUseridByToken("valid-token");
-//        doReturn(testRecipe).when(apiService).getHostRecipe(testUser);
-//    }
-//
+package ch.uzh.ifi.hase.soprafs23.controller;
+
+import ch.uzh.ifi.hase.soprafs23.SpooncularAPI.*;
+import ch.uzh.ifi.hase.soprafs23.entity.Group;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.service.GroupService;
+import ch.uzh.ifi.hase.soprafs23.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.NestedServletException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(RestTemplateConfig.class)
+public class APIControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GroupService groupService;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private APIService apiService;
+    @Mock
+    private RestTemplate restTemplate;
+    @InjectMocks
+    private APIController apiController;
+
+    private Group testGroup;
+    private User testUser;
+    private Recipe testRecipe;
+
+    @BeforeEach
+    public void setUp() {
+        testGroup = new Group();
+        testGroup.setId(1L);
+        testGroup.setHostId(1L);
+
+        testUser = new User();
+        testUser.setId(1L);
+
+        testRecipe = new Recipe();
+        testRecipe.setId(1);
+        testRecipe.setTitle("Test Recipe");
+        testRecipe.setReadyInMinutes(30);
+
+        doReturn(testGroup).when(groupService).getGroupById(anyLong());
+        doReturn(testUser).when(userService).getUserById(anyLong());
+        doReturn(1L).when(userService).getUseridByToken("valid-token");
+        doReturn(testRecipe).when(apiService).getHostRecipe(testUser);
+    }
+
+
 //    @Test
 //    public void getRandomRecipe_success_200() throws Exception {
 //        mockMvc.perform(get("/groups/{groupId}/result", 1L)
@@ -240,5 +240,5 @@
 //            assertEquals("422 UnprocessableEntity", cause.getMessage());
 //        }
 //    }
-//
-//}
+
+}
