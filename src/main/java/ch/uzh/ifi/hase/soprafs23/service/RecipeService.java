2742918f2c29016d3a6196b1c72a064706ec5cbe
Lany Weizenblut <lany1997@hotmail.com>
2023-05-12 11:45:41 +0200

    remove some unused imports

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/RecipeService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/RecipeService.java
@@ -1,10 +1,7 @@
 package ch.uzh.ifi.hase.soprafs23.service;
 
 import ch.uzh.ifi.hase.soprafs23.SpooncularAPI.Recipe;
-import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.entity.Group;
-import ch.uzh.ifi.hase.soprafs23.service.GroupService;
-import ch.uzh.ifi.hase.soprafs23.SpooncularAPI.IngredientAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ch.uzh.ifi.hase.soprafs23.repository.RecipeRepository;
