5cd1e0dd6801d235c56e8f0c41a8b5ce500b01a8
Lany Weizenblut <lany1997@hotmail.com>
2023-05-25 12:13:20 +0200

    bring back import

--- src/main/java/ch/uzh/ifi/hase/soprafs23/SpooncularAPI/APIController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/SpooncularAPI/APIController.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.hase.soprafs23.SpooncularAPI;
 
+import ch.uzh.ifi.hase.soprafs23.entity.FullIngredient;
 import ch.uzh.ifi.hase.soprafs23.constant.GroupState;
 import ch.uzh.ifi.hase.soprafs23.entity.Group;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
