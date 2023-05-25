8f5a13bfe9ef150b6d39f780b62eb1b29f97d758
akkuebler <91547040+akkuebler@users.noreply.github.com>
2023-05-25 11:50:24 +0200

    add fullIngredientRep again

--- src/main/java/ch/uzh/ifi/hase/soprafs23/SpooncularAPI/APIController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/SpooncularAPI/APIController.java
@@ -50,6 +50,9 @@ public class APIController {
     private UserService userService;
 
     @Autowired
+    private FullIngredientRepository fullIngredientRepository;
+
+    @Autowired
     private RecipeService recipeService;
 
     @GetMapping("/groups/{groupId}/result")
